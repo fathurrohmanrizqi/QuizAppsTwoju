d270f8d429e54229399f9373102675805ae35b6a
Rizqi Fathurrohman <fathurrohmanrizqi3@gmail.com>
2025-05-26 11:47:14 +0700

    Initial commit

--- app/src/main/java/com/fathurrohman/piquizapps/HomeActivity.kt
+++ app/src/main/java/com/fathurrohman/piquizapps/HomeActivity.kt
@@ -3,10 +3,13 @@ package com.fathurrohman.piquizapps
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,30 @@ class HomeActivity : AppCompatActivity() {
         val btnBelajar = findViewById<Button>(R.id.btnBelajar)
         val btnQuiz = findViewById<Button>(R.id.btnQuiz)
         val btnHistoryQuiz = findViewById<Button>(R.id.btnHistoryQuiz)
+        val txtName = findViewById<TextView>(R.id.txtName)
+        val txtClass = findViewById<TextView>(R.id.txtClass)
+
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        val db = FirebaseFirestore.getInstance()
+        if (uid != null) {
+            db.collection("users").document(uid).get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        val name = document.getString("name")
+                        val kelas = document.getString("class")
+
+                        txtName.text = name
+                        txtClass.text = kelas
+                    } else {
+                        txtName.text = "Nama tidak ditemukan"
+                        txtClass.text = "Kelas tidak ditemukan"
+                    }
+                }
+                .addOnFailureListener {
+                    txtName.text = "Gagal mengambil nama"
+                    txtClass.text = "Gagal mengambil kelas"
+                }
+        }
 
         // Aksi klik tombol Materi Pembelajaran
         btnBelajar.setOnClickListener {
